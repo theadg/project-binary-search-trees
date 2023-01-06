d84975e12e3e934e44b8e4d77c9320c6a3986d6f
theadg <deguzmanbernardandrew@gmail.com>
2023-01-06 22:21:32 +0800

    Add levelOrder fn

--- index.js
+++ index.js
@@ -88,26 +88,67 @@ const find = (tree, value) => {
   }
 };
 
-// const getArrayValues = (tree) => {
-//   const array = getNodeValue(tree);
+// TODO: a iteration(loop) version
+const levelOrder = (tree, cb = null, arr = []) => {
+  // traverse tree in a breadth first level
+  // first return the level order array
+  if (tree !== null) {
+    if (tree.data && !arr.includes(tree.data)) {
+      arr.push(tree.data);
+      cb(tree.data);
+    }
 
-//   // array.forEach((element) => newTree.push(element));
+    if (tree.left !== null && tree.left.data && !arr.includes(tree.left.data)) {
+      arr.push(tree.left.data);
+      cb(tree.left.data);
+    }
 
-//   console.log(typeof array);
-// };
+    if (
+      tree.right !== null &&
+      tree.right.data &&
+      !arr.includes(tree.right.data)
+    ) {
+      arr.push(tree.right.data);
+      cb(tree.right.data);
+    }
+
+    levelOrder(tree.left, cb, arr);
+    levelOrder(tree.right, cb, arr);
+
+    // return removeDuplicates(arr);
+    return arr;
+  }
+};
+
 // Test code
 // createNode
 // const testNode = createNode(5);
 // console.log(testNode);
 
 // buildTree
-// const testTree = buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-const testTree = buildTree([1, 2, 3, 4, 5]);
-// getArrayValues(testTree);
+const testTree = buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log(prettyPrint(testTree));
+// const testTree = buildTree([1, 2, 3, 4, 5]);
+// console.log(getArrayValues(testTree));
+console.log(preOrder(testTree));
+console.log(inOrder(testTree));
+console.log(postOrder(testTree));
+
 // console.log(prettyPrint(insert(testTree, 0)));
 // console.log(prettyPrint(remove(testTree, 0)));
-console.log(find(testTree, 4));
+// console.log(find(testTree, 4));
+// const transformedTree = transformTree(testTree);
+// levelOrder(testTree, print);
+// console.log(transformedTree.length);
+// console.log(transformedTree.shift());
+// console.log(transformedTree);
+// console.log(transformTree(testTree));
+
 // find(testTree, 2);
 // console.log(getNodeValue(testTree));
 // console.log(prettyPrint(testTree));
 // console.log(testTree);
+
+// console.log(prettyPrint(testTree));
+// console.log(getArrayValues(inOrder(testTree)));
+// console.log(getArrayValues(postOrder(testTree)));
