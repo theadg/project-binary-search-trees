5740395356a22e5cfe11625ab828b7320925c6c3
theadg <deguzmanbernardandrew@gmail.com>
2023-01-06 16:37:48 +0800

    Add insert and getArrayValues fn

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ const Tree = (arr = []) => {
 };
 
 const buildTree = (arr, start = 0, end = arr.length - 1) => {
-  //
+  //sort the array first
   arr = removeDuplicates(sort(arr));
+  //   base case
   if (start > end) return null;
+  //   get the middle index of the array
   const mid = parseInt((start + end) / 2);
 
+  // create a node based on the index of the array;
   const root = createNode(arr[mid]);
   root.left = buildTree(arr, start, mid - 1);
   root.right = buildTree(arr, mid + 1, end);
@@ -46,12 +49,44 @@ const sort = (arr) => {
 const removeDuplicates = (arr) => {
   return [...new Set(arr)];
 };
+
+const getArrayValues = (node, arr = []) => {
+  if (node !== null) {
+    arr.push(node.data);
+    getArrayValues(node.left, arr);
+    getArrayValues(node.right, arr);
+
+    return arr;
+  }
+};
+
+const insert = (tree, value) => {
+  let array = getArrayValues(tree);
+  array.push(value);
+  array = sort(removeDuplicates(array));
+  return buildTree(array);
+};
+
+// const getArrayValues = (tree) => {
+//   const array = getNodeValue(tree);
+
+//   // array.forEach((element) => newTree.push(element));
+
+//   console.log(typeof array);
+// };
 // Test code
 // createNode
 // const testNode = createNode(5);
 // console.log(testNode);
 
 // buildTree
-const testTree = buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+// const testTree = buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const testTree = buildTree([1, 2, 3, 4, 5]);
+// getArrayValues(testTree);
+// console.log(prettyPrint(insert(testTree, 0)));
+// console.log(prettyPrint(remove(testTree, 0)));
+console.log(find(testTree, 4));
+// find(testTree, 2);
+// console.log(getNodeValue(testTree));
 // console.log(prettyPrint(testTree));
-console.log(testTree);
+// console.log(testTree);
